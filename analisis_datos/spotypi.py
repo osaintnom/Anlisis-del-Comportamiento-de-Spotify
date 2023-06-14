94f53dd5d8974337ceac7b8bcb0656aa065c516f
Olivier Saint Nom <oltie4002@gmail.com>
2023-06-13 22:31:42 -0300

    mejoras index con problemas

--- analisis_datos/spotypi.py
+++ analisis_datos/spotypi.py
@@ -73,10 +73,13 @@ def main():
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    df = convert_json_into_df(results=sp.playlist('27x92YF2JFoLHatzvvqn3M'), sp=sp)
-    df.to_csv("./VDATA/VD_FINAL/analisis_datos/Afterlife.csv", index=False)    
+    df = convert_json_into_df(results=sp.playlist('42g5kM4VWLmIeoLfmGzSUq'), sp=sp)
+    df.to_csv("./analisis_datos/gerar.csv", index=False)    
 
 if __name__ == '__main__':
     main()
 
 #https://open.spotify.com/playlist/7cLua2vBmBZsQQYoPXNduD?si=525a256de69a4563
+# oli = https://open.spotify.com/playlist/2yCABoqVYJ36dwvPSyry18?si=2651cbbd4ebe4592
+# valen = 
+# gerar = https://open.spotify.com/playlist/42g5kM4VWLmIeoLfmGzSUq?si=57df4f58d7ea46f4
